829683d3b0443d1be1399669e36c27877fb39bea
parkerk8 <12446200-parkerk8@users.noreply.replit.com>
2023-04-22 18:03:58 +0000

    Initial commit

---
+++ discord_bot.py
@@ -0,0 +1,67 @@
+import discord
+from discord.ext import commands
+import amazon_sku_tracker
+
+DISCORD_BOT_TOKEN = "MTA5ODM3NTk4NzAyNjIxNTA5Mg.GS8CdI.DP07l4UkpgD4twepjR42A4dIJZn2mUMC05CT-o"  # Replace with your bot token
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.guild_messages = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command(name='add_storefront')
+async def add_storefront(ctx, storefront_url: str):
+    if storefront_url not in amazon_sku_tracker.storefront_links:
+        amazon_sku_tracker.storefront_links.append(storefront_url)
+        response = f"Added new storefront: {storefront_url}"
+    else:
+        response = f"Storefront {storefront_url} is already being tracked."
+    await ctx.send(response)
+
+@bot.event
+async def on_ready():
+    print("Bot is ready.")
+
+print("Bot is starting...")
+bot.run(DISCORD_BOT_TOKEN)
+import os
+import discord
+from discord.ext import commands, tasks
+import amazon_sku_tracker
+
+DISCORD_BOT_TOKEN = "MTA3NjIxMTIwNjUzMDAyNzY5MA.GW5FGG.ie82903vt1jjTnQZegQBlQGEbiZstZT9bzs0XU"  # Get your bot token from an environment variable
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.guild_messages = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command(name='add_storefront')
+async def add_storefront(ctx, storefront_url: str):
+    if storefront_url not in amazon_sku_tracker.storefront_links:
+        amazon_sku_tracker.storefront_links.append(storefront_url)
+        response = f"Added new storefront: {storefront_url}"
+    else:
+        response = f"Storefront {storefront_url} is already being tracked."
+    await ctx.send(response)
+
+@tasks.loop(seconds=60)  # Adjust the interval to your preference
+async def sku_tracking_job():
+    # Replace CHANNEL_ID with the ID of the Discord channel you want to send notifications to
+    channel = bot.get_channel(509234451033620484)
+    if channel:
+        await amazon_sku_tracker.job(channel)
+    else:
+        print("Failed to find the Discord channel.")
+
+@bot.event
+async def on_ready():
+    print("Bot is ready.")
+    sku_tracking_job.start()
+
+print("Bot is starting...")
+bot.run(DISCORD_BOT_TOKEN)
---
+++ main.py
@@ -0,0 +1,3 @@
+import amazon_sku_tracker
+import discord_bot
+discord_bot.bot.run(discord_bot.DISCORD_BOT_TOKEN)
