6eedd9b2d07afd66438fac6ccf5c03eef0459dad
parkerk8 <12446200-parkerk8@users.noreply.replit.com>
2023-04-22 23:02:48 +0000

    Added correct intents
    Intents were missing in discord_bot.py, this fixed the reponses in the discord server

--- amazon_sku_tracker.py
+++ amazon_sku_tracker.py
@@ -6,55 +6,62 @@ import os
 # Configuration
 storefront_links = []
 
+
 def get_product_skus(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
-    }
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
+  headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+  }
+  response = requests.get(url, headers=headers)
+  soup = BeautifulSoup(response.text, 'html.parser')
 
-    product_links = soup.find_all('a', class_='a-link-normal', href=True)
-    
-    skus = set()
-    for link in product_links:
-        product_url = link['href']
-        sku = product_url.split('/')[-1]
-        skus.add(sku)
+  product_links = soup.find_all('a', class_='a-link-normal', href=True)
 
-    return skus
+  skus = set()
+  for link in product_links:
+    product_url = link['href']
+    sku = product_url.split('/')[-1]
+    skus.add(sku)
+
+  return skus
+
 
 def load_old_skus():
-    if os.path.exists('old_skus.json'):
-        with open('old_skus.json', 'r') as file:
-            old_skus = json.load(file)
-    else:
-        old_skus = {}
-    return old_skus
+  if os.path.exists('old_skus.json'):
+    with open('old_skus.json', 'r') as file:
+      old_skus = json.load(file)
+  else:
+    old_skus = {}
+  return old_skus
+
 
 def save_new_skus_as_old(url, new_skus):
-    old_skus = load_old_skus()
-    old_skus[url] = list(new_skus)
-    with open('old_skus.json', 'w') as file:
-        json.dump(old_skus, file)
+  old_skus = load_old_skus()
+  old_skus[url] = list(new_skus)
+  with open('old_skus.json', 'w') as file:
+    json.dump(old_skus, file)
+
 
 def check_for_new_skus(url):
-    new_skus = get_product_skus(url)
-    old_skus_data = load_old_skus()
-    old_skus = set(old_skus_data.get(url, []))
+  new_skus = get_product_skus(url)
+  old_skus_data = load_old_skus()
+  old_skus = set(old_skus_data.get(url, []))
 
-    new_added_skus = new_skus.difference(old_skus)
+  new_added_skus = new_skus.difference(old_skus)
 
-    # Save new_skus as old_skus for the next check
-    save_new_skus_as_old(url, new_skus)
+  # Save new_skus as old_skus for the next check
+  save_new_skus_as_old(url, new_skus)
 
-    return new_added_skus
+  return new_added_skus
+
 
 async def send_discord_notification(channel, new_skus):
-    message = 'The following SKUs have been added:\n' + '\n'.join(new_skus)
-    await channel.send(message)
+  message = 'The following SKUs have been added:\n' + '\n'.join(new_skus)
+  await channel.send(message)
+
 
 async def job(channel):
-    for url in storefront_links:
-        new_skus = check_for_new_skus(url)
-        if new_skus:
-            await send_discord_notification(channel, new_skus)
+  for url in storefront_links:
+    new_skus = check_for_new_skus(url)
+    if new_skus:
+      await send_discord_notification(channel, new_skus)
--- discord_bot.py
+++ discord_bot.py
@@ -3,41 +3,45 @@ import discord
 from discord.ext import commands, tasks
 import amazon_sku_tracker
 
-my_secret = os.environ['DISCORD_BOT_TOKEN']  # Get your bot token from an environment variable
+secret_key = os.environ["DISCORD_BOT_TOKEN"]  # Get your bot token from an environment variable
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
-intents.guild_messages = True
+intents.messages = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
 @bot.command(name='add_storefront')
 async def add_storefront(ctx, storefront_url: str):
-  if storefront_url not in amazon_sku_tracker.storefront_links:
-    amazon_sku_tracker.storefront_links.append(storefront_url)
-    response = f"Added new storefront: {storefront_url}"
-  else:
-    response = f"Storefront {storefront_url} is already being tracked."
-  await ctx.send(response)
-
+    print(f"Processing !add_storefront command for {storefront_url}")
+    if storefront_url not in amazon_sku_tracker.storefront_links:
+        amazon_sku_tracker.storefront_links.append(storefront_url)
+        response = f"Added new storefront: {storefront_url}"
+    else:
+        response = f"Storefront {storefront_url} is already being tracked."
+    await ctx.send(response)
 
 @tasks.loop(seconds=60)  # Adjust the interval to your preference
 async def sku_tracking_job():
-  # Replace CHANNEL_ID with the ID of the Discord channel you want to send notifications to
-  channel = bot.get_channel(509234451033620484)
-  if channel:
-    await amazon_sku_tracker.job(channel)
-  else:
-    print("Failed to find the Discord channel.")
-
+    # Replace CHANNEL_ID with the ID of the Discord channel you want to send notifications to
+    channel = bot.get_channel(509234451033620482)
+    if channel:
+        await amazon_sku_tracker.job(channel)
+    else:
+        print("Failed to find the Discord channel.")
 
 @bot.event
 async def on_ready():
-  print("Bot is ready.")
-  sku_tracking_job.start()
+    print("Bot is ready.")
 
+@bot.event
+async def on_command_error(ctx, error):
+    print(f"Error occurred while processing command: {error}")
+    await ctx.send(f"An error occurred: {error}")
 
 print("Bot is starting...")
-bot.run(my_secret)
+bot.run(secret_key)
+
+sku_tracking_job.start()
